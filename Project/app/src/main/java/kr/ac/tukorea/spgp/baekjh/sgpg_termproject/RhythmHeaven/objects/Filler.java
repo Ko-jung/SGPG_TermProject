34134d72baef6c5f87650eda7fb80ee6a6f76462
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2024-06-11 04:20:18 +0900

    위치 조정 및 Frame 축소

--- Project/app/src/main/java/kr/ac/tukorea/spgp/baekjh/sgpg_termproject/RhythmHeaven/objects/Filler.java
+++ Project/app/src/main/java/kr/ac/tukorea/spgp/baekjh/sgpg_termproject/RhythmHeaven/objects/Filler.java
@@ -13,7 +13,7 @@ public class Filler extends SheetSprite {
     protected State state = State.idle;
     protected static Rect[][] srcRectsArray = {
             makeRects(28),      // State.idle
-            makeRects(28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,29), // State.TouchDown
+            makeRects(25,19,13,7,1,29), // State.TouchDown
             makeRects(1,2,3,2,1) // State.Filling.
 
     };
@@ -24,7 +24,7 @@ public class Filler extends SheetSprite {
         this.height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
         //setPosition(0, 0, Metrics.width, height);
 
-        setPosition(4.5f, 8.0f, 9.f, 16.0f);
+        setPosition(4.5f, 11.0f, 9.f, 16.0f);
         srcRects = srcRectsArray[state.ordinal()];
     }
     protected static Rect[] makeRects(int... indices) {
@@ -52,9 +52,12 @@ public class Filler extends SheetSprite {
     }
 
     public boolean onTouchEvent(MotionEvent e){
-        state = State.touchDown;
-        srcRects = srcRectsArray[state.ordinal()];
-        createdOn = System.currentTimeMillis();
+        if(e.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            state = State.touchDown;
+            srcRects = srcRectsArray[state.ordinal()];
+            createdOn = System.currentTimeMillis();
+        }
         return true;
     }
 }
